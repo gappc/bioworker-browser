15928995487a8e060ebf13b2ba545450e20b8c17
Christian Gapp <gapp.christian@gmail.com>
2014-10-29 22:58:04 +0100

    Changes now working with biohadoop-0.4.0-SNAPSHOT. Implemented tiled matrix multiplication (in col format) of biohadoop-algorithms-0.4.0-SNAPSHOT

--- js/worker.js
+++ js/worker.js
@@ -17,46 +17,22 @@ $(function() {
 	});
 
 	// add new worker
-	$("#startWorker")
-			.on(
-					"click",
-					function() {
-						var url = $("#workerUrl").val();
-						var type = $("#workerType").val()
-						if (type === "rest") {
-							alert("Currently not supported");
-							return;
-							// if (!isUrlValid(url)) {
-							// addAlert($("#startWorker"), "Invalid URL: "
-							// + url);
-							// return;
-							// }
-							//
-							// var startResult = startRestWorker(url)
-							// if (startResult == false) {
-							// }
-							// startResult.done(function() {
-							// console.log("OK");
-							// }).fail(
-							// function(a, b, c) {
-							// addAlert($("#startWorker"),
-							// "Error while registering to master at: "
-							// + url);
-							// }).always(function() {
-							//								console.log("FINISHED");
-							//							})
-						} else if (type === "websocket") {
-							if (!isUrlValid(url)) {
-								addAlert($("#startWorker"), "Invalid URL: "
-										+ url);
-								return;
-							}
-							startWebSocketWorker(url);
-						} else {
-							addAlert($("#startWorker"),
-									"Worker type is not supported");
-						}
-					});
+	$("#startWorker").on("click", function() {
+		var url = $("#workerUrl").val();
+		var type = $("#workerType").val()
+		if (type === "rest") {
+			alert("Currently not supported");
+			return;
+		} else if (type === "websocket") {
+			if (!isUrlValid(url)) {
+				addAlert($("#startWorker"), "Invalid URL: " + url);
+				return;
+			}
+			startWebSocketWorker(url);
+		} else {
+			addAlert($("#startWorker"), "Worker type is not supported");
+		}
+	});
 })
 
 function addAlert(sibling, text) {
@@ -67,22 +43,6 @@ function addAlert(sibling, text) {
 
 function isUrlValid(url) {
 	return (url != null && url.length > 0);
-}
-
-function startRestWorker(url) {
-	var data = registrationRequest();
-	// var callback = $.post(url, data);
-
-	var callback = $.ajax({
-		type : "POST",
-		url : url,
-		data : registrationRequest(),
-		dataType : "json",
-		origin : "FUCK",
-		crossDomain : true
-	});
-
-	return callback;
 }
 
 function startWebSocketWorker(url) {
@@ -97,12 +57,14 @@ function startWebSocketWorker(url) {
 		url : url,
 		type : "WebSocket",
 		state : "new",
-		count : 0
+		count : 0,
+		initialData : {},
+		oldTask : null
 	}
 	updateReport(connection.worker);
 
 	connection.onopen = function() {
-		connection.send(workInitRequest());
+		connection.send(workRequest(null));
 		connection.worker.state = "connected";
 		updateReport(connection.worker);
 	};
@@ -110,46 +72,35 @@ function startWebSocketWorker(url) {
 	// Log messages from the server
 	connection.onmessage = function(message) {
 		var inputMessage = JSON.parse(message.data);
-		if (inputMessage.type == "REGISTRATION_RESPONSE") {
-			// Not needed in the case of sum
-			connection.send(workInitRequest());
-			connection.worker.state = "registration";
-			updateReport(connection.worker);
-		} else if (inputMessage.type == "WORK_INIT_RESPONSE") {
-			if (!checkResponseType(inputMessage)) {
-				connection.worker.state = "error";
-				updateReport(connection.worker);
-			} else {
-				connection.worker.state = "init";
-				updateReport(connection.worker);
-
-				var data = inputMessage.task[1].data[1];
-
-				var computationResult = computeResult(data);
-				var resultMessage = workRequest(inputMessage, computationResult);
-				connection.send(resultMessage);
-
-				connection.worker.state = "running";
-				updateReport(connection.worker);
-				connection.worker.count++;
+		var task = inputMessage.task;
+		var initialData = null;
+		// Work response
+		if (inputMessage.type == 1) {
+			var taskTypeId = task.taskTypeId;
+			var initialData = connection.worker.initialData[taskTypeId];
+			if (initialData == null) {
+				connection.worker.oldTask = task;
+				connection.send(initialDataRequest(task));
+				return;
 			}
-		} else if (inputMessage.type == "WORK_RESPONSE") {
-			var task = inputMessage.data;
-			var computationResult = computeResult(task);
-			var resultMessage = workRequest(message, computationResult);
-			connection.send(resultMessage);
-
-			connection.worker.count++;
-			connection.worker.state = "running";
-			if (connection.worker.count % 1000 == 0) {
-				updateReport(connection.worker);
-			}
-		} else if (inputMessage.type == "SHUTDOWN") {
-			console.log("Got SHUTDOWN");
-			connection.worker.state = "finished";
-			updateReport(connection.worker);
-			connection.close();
+		} else if (inputMessage.type == 3) {
+			var taskTypeId = inputMessage.task.taskTypeId;
+			var task = inputMessage.task;
+			var dataType = task.data[0];
+			// TODO: Check for dataType
+			var asyncClassName = task.data[1]["asyncComputableClassName"];
+			initialData = task.data[1]["initialData"];
+			connection.worker.initialData[taskTypeId] = initialData;
+			task = connection.worker.oldTask;
 		}
+		var start = new Date().getMilliseconds();
+		var result = computeResult(task.data, initialData);
+		var end = new Date().getMilliseconds();
+		task.data = [ "java.lang.Long", (end - start) * 1000000 ];
+		connection.send(workRequest(task));
+		connection.worker.count++;
+		connection.worker.state = "running";
+		updateReport(connection.worker);
 	};
 
 	// Log errors
@@ -173,61 +124,53 @@ function startWebSocketWorker(url) {
 	}
 }
 
-function registrationRequest() {
+function workRequest(task) {
 	var message = {
-		type : "REGISTRATION_REQUEST",
-		data : null
-	}
-	return JSON.stringify(message);
-}
-
-function workInitRequest() {
-	var message = {
-		type : "WORK_INIT_REQUEST",
-		task : null
-	}
-	return JSON.stringify(message);
-}
-
-function workRequest(inputMessage, data) {
-	var message = inputMessage;
-	message.type = "WORK_REQUEST";
-	message.task[1].data = data
-
+		type : 0,
+		task : task
+	};
 	var result = JSON.stringify(message);
-
 	return result;
 }
 
-function checkResponseType(response) {
-	if (!$.isArray(response.task)) {
-		console.log("Response is not in the right format, must have a task property, that is of type array");
-		return false;
-	}
-	if (!response.task.length >= 2) {
-		console.log("Response is not in the right format, task array must be of size >= 2");
-		return false;
-	}
-	if (response.task[0] != "at.ac.uibk.dps.biohadoop.tasksystem.queue.ClassNameWrappedTask") {
-		console.log("Response is not in the right format, task array's first entry must be the string \"at.ac.uibk.dps.biohadoop.tasksystem.queue.ClassNameWrappedTask\"");
-		return false;
-	}
-	var taskWrapper = response.task[1];
-	if (taskWrapper.className != "at.ac.uibk.dps.biohadoop.algorithms.sum.AsyncSumComputation") {
-		console.log("Can not compute task of type "
-						+ taskWrapper.className
-						+ "; can only compute tasks of type \"at.ac.uibk.dps.biohadoop.algorithms.sum.AsyncSumComputation\"");
-		return false;
-	}
-	return true;
+function initialDataRequest(task) {
+	var message = {
+		type : 2,
+		task : task
+	};
+	var result = JSON.stringify(message);
+	return result;
 }
 
-function computeResult(data) {
-	var sum = 0;
-	for (var i = 0; i < data.length; i++) {
-		sum += data[i];
+function computeResult(data, initialData) {
+	var matrixA = initialData[1]["matrixA"];
+	var matrixB = initialData[1]["matrixB"];
+	var size = matrixA.length;
+	var matrixC = new Array(size);
+	for (var i = 0; i < size; i++) {
+		matrixC[i] = new Array(size);
+		for (var j = 0; j < size; j++) {
+			matrixC[i][j] = 0;
+		}
 	}
-	return sum;
+	var blocks = data[1];
+	for (var iOuter = 0; iOuter < size; iOuter += blocks[0]) {
+		for (var jOuter = 0; jOuter < size; jOuter += blocks[1]) {
+			for (var kOuter = 0; kOuter < size; kOuter += blocks[0]) {
+				for (var i = iOuter; i < Math.min(iOuter + blocks[0], size); i++) {
+					for (var j = jOuter; j < Math.min(jOuter + blocks[1], size); j++) {
+						for (var k = kOuter; k < Math.min(kOuter + blocks[0],
+								size); k++) {
+							// console.log("doing [" + i + "][" + j + "]");
+							matrixC[i][j] += matrixA[i][k] * matrixB[j][k];
+							// console.log("done [" + i + "][" + j + "]");
+						}
+					}
+				}
+			}
+		}
+	}
+	return matrixC;
 }
 
 function updateReport(worker) {
@@ -236,12 +179,24 @@ function updateReport(worker) {
 	// check if row exists
 	if (tds.length == 0) {
 		var newRow = $(""
-				+ "<tr id='worker-" + worker.id + "'>"
-				+ "<td>" + worker.id + "</td>"
-				+ "<td>" + worker.url + "</td>"
-				+ "<td>" + worker.type + "</td>"
-				+ "<td>" + worker.state + "</td>"
-				+ "<td>" + worker.count + "</td>"
+				+ "<tr id='worker-"
+				+ worker.id
+				+ "'>"
+				+ "<td>"
+				+ worker.id
+				+ "</td>"
+				+ "<td>"
+				+ worker.url
+				+ "</td>"
+				+ "<td>"
+				+ worker.type
+				+ "</td>"
+				+ "<td>"
+				+ worker.state
+				+ "</td>"
+				+ "<td>"
+				+ worker.count
+				+ "</td>"
 				+ "<td><span class='glyphicon glyphicon-remove' style='cursor:pointer'></span></td>"
 				+ "</tr>");
 		$("#reports #running tbody").append(newRow);
